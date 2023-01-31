d54e3a5d45d5e6d27d408bf901afef2f7d4e37ae
lenac0des <ellenajean@gmail.com>
2023-01-31 13:57:06 -0500

    image render text

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -16,9 +16,27 @@
 // the mymessage component will only accept one prop called message
 // underneath the prop add an if check that will ask if our message is the actual text message or an image
 // if it is greater than zero then the messgae is an image if(message?.attachments?.length > 0)
+// if that is the case then we will have to return something different
+// return an image selfclosing tag and add props to it
+// first we will add the src which will take the message grab the first attchment[0]and then the file
+// then the alt which will state the name message attachment
+// then add the className set it to message image'
+// finally inline styles which will have the message float to the right
 
 const MyMessage = ({ message }) => {
-    if(message?.attachments?.length > 0)
+    // this will render an image if the message is an image
+    if(message?.attachments?.length > 0) {
+        return (
+            <img 
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right' }}
+
+            />
+        )
+    }
+
     return(
         <div>
             MyMessage
