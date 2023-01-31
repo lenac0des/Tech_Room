183d1590372151dc0c9a62d57f836d509e390c03
lenac0des <ellenajean@gmail.com>
2023-01-31 10:15:02 -0500

    imported chat engine and css file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 31/01/2023 - 10:13:01
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 31/01/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
