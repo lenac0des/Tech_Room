76c2c192431ff2d0fada7a8527d17493a1e91456
lenac0des <ellenajean@gmail.com>
2023-01-31 13:58:56 -0500

    notes

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -13,7 +13,8 @@
 
 // create funtional component for chat bubble
 
-//  image render
+//  image render notes
+
 // the mymessage component will only accept one prop called message
 // underneath the prop add an if check that will ask if our message is the actual text message or an image
 // if it is greater than zero then the messgae is an image if(message?.attachments?.length > 0)
@@ -26,7 +27,7 @@
 
 
 
-// Text render
+// Text render notes
 
 const MyMessage = ({ message }) => {
     // this will render an image if the message is an image
