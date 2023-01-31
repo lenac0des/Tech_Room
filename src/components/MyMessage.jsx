91f3aace5fb937ae054b714ad40de7c64baddec2
lenac0des <ellenajean@gmail.com>
2023-01-31 11:38:22 -0500

    create funtional component for chat bubble

---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,24 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 31/01/2023 - 11:37:19
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 31/01/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+
+// create funtional component for chat bubble
+
+const MyMessage () => {
+    return(
+        <div>
+            MyMessage
+        </div>
+    );
+}
+
+export default MyMessage;
