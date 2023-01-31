d48eed4e5131305a5b844d16c781a8401b04c347
lenac0des <ellenajean@gmail.com>
2023-01-31 10:07:23 -0500

    renamed react app and set the boiler plate for react

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 31/01/2023 - 10:02:03
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 31/01/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('root'));
