fb868861d5e8d2d6f54c3a5de4e49f1112c0dd8d
lenac0des <ellenajean@gmail.com>
2023-01-31 11:33:31 -0500

    created components\

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,29 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 31/01/2023 - 11:22:31
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 31/01/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+// create a functional componenet
+// the chat feed will use 3 different componemts which I will create
+// first componment is messageforms.jsx
+
+
+const ChatFeed = (props) => {
+    console.log(props);
+
+    return (
+        <div>
+            ChatFeed
+
+        </div>
+    );
+}
+
+export default ChatFeed;
