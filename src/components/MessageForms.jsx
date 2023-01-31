abd67d43c338a4b3f5d5868feabc27dfbc7c34cb
lenac0des <ellenajean@gmail.com>
2023-01-31 11:36:08 -0500

    functional component

---
+++ src/components/MessageForms.jsx
@@ -0,0 +1,23 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 31/01/2023 - 11:33:42
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 31/01/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+// create another functional componment
+
+const MessageForm () => {
+    return(
+        <div>
+            MessageForm
+        </div>
+    );
+}
+
+export default MessageForm;
